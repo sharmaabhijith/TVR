64d38662706200a3cee4f35d0ae2496ce60a3216
sharmaabhijith <sharmaabhijith1@gmail.com>
2023-08-01 20:15:20 -0700

    new add

--- test.py
+++ test.py
@@ -51,24 +51,6 @@ netG.eval()
 
 ##-------------------------------------------------------------------------------------------------------------------------------------------------------##
 
-st.set_page_config(layout="wide", page_title="Image Background Remover")
-
-st.write("# Total Variation based Image Resurfacer (TVR)")
-st.write("## Who am I?")
-st.markdown(
-    "*A model-agnostic defense against single and multi patch attacks based on total variation for image resurfacing (TVR). The TVR is an image-cleansing method that processes images to remove probable adversarial regions.*"
-)
-st.sidebar.write("CHALLENGE ME IF YOU CAN! :gear:")
-
-opt_model_name = st.sidebar.selectbox('PLEASE SELECT A CNN MODEL', ('Alexnet', 'ResNet18', 'SqueezeNet', 'VGG16', 'GoogleNet', 'Inception_v3'))
-#st.sidebar.write('You selected:', opt_model_name)
-
-opt_adv_patch = st.sidebar.selectbox('## PLEASE SELECT AN ADVERSARIAL PATCH', ('soap_dispenser', 'cornet', 'plate', 'banana', 'cup', 'typewriter', 'electric_guitar', 'hair_spray', 'sock', 'cellular_telephone'))
-#st.sidebar.write('You selected:', opt_adv_patch)
-
-opt_block_size = st.sidebar.selectbox('## PLEASE SELECT A BLOCK-SIZE', (7, 14, 28, 56, 112))
-#st.sidebar.write('You selected:', opt_block_size)
-
 # Download the fixed image
 def convert_image(img):
     buf = BytesIO()
@@ -104,22 +86,11 @@ def cleanse_image(img_tensor, block_size, netG):
 
 def fix_image(upload, block_size, netG):
     image = Image.open(upload)
-    col1.write("### Adversarial Image :camera:")
-    col1.image(image)
-
     img_tensor = convert_img_2_tensor(image)
+    print(img_tensor.shape)
     cleansed_img_tensor = cleanse_image(img_tensor, block_size, netG)
     cleansed_image = convert_tensor_2_img(cleansed_img_tensor)
-    col2.write("### Cleansed Image :wrench:")
-    col2.image(cleansed_image)
-    st.sidebar.markdown("\n")
-    st.sidebar.download_button("DOWNLOAD CLEANSED IMAGE", convert_image(fixed), "fixed.png", "image/png")
+    print(cleansed_img_tensor.shape)
 
-col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("## UPLOAD IMAGE TO BE ATTACKED", type=["png", "jpg", "jpeg"])
-
-if my_upload is not None:
-    fix_image(upload=my_upload, block_size=opt_block_size, netG=netG)
-#else:
-#    fix_image("./Figures/Placeholder_view_vector.png")
+fix_image('./Figures/Placeholder_view_vector.png',28, netG)
 
